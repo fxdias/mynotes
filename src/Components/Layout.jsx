eee01535ccb57fe36f4e66a465e1e2c16caec10b
Fernando Ximenes <fxdias@gmail.com>
2022-10-17 18:38:55 -0300

    add layout appbar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -3,9 +3,9 @@ import { css } from '@emotion/react'
 import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material'
 import SubjectIcon from '@mui/icons-material/Subject';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import Drawer from '@mui/material/Drawer'
-import Typography from '@mui/material/Typography'
+import { Drawer, Typography, AppBar, Toolbar } from '@mui/material'
 import { useNavigate, useLocation } from 'react-router-dom'
+
 
 const drawerWidth = 240
 const menuItems = [
@@ -25,12 +25,22 @@ function Layout({ children }) {
     const location = useLocation()
     return (
         <div css={css`display: flex;`}>
+            <AppBar css={css`
+                width: calc(100% - ${drawerWidth}px);
+            `}
+            >
+                <Toolbar>
+                    <Typography>
+                        Welcome to the Mynotes
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             <Drawer sx={{ width: drawerWidth, ".MuiDrawer-paper": { width: drawerWidth } }}
                 variant="permanent"
                 anchor="left"
             >
                 <div>
-                    <Typography variant="h5">Net Notes</Typography>
+                    <Typography variant="h5">MyNotes</Typography>
                 </div>
                 <List>
                     {menuItems.map(item => (
@@ -50,6 +60,7 @@ function Layout({ children }) {
             <div css={css`
                 background: #f9f9f9;
                 width: 100%;
+                padding-top: 100px;
             `}
             >
                 {children}
